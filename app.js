0c0939cb717720e06a70f5ee9631731a0d5260a8
DanielGG1024 <a22472510@gmail.com>
2021-07-07 17:12:36 +0800

    html layout ant main

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const port = 3000
-app.engine('handlebars',exphbs({defaultLayout:'main'}))
-app.set('view engine','handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.render('index')
 })
-
+app.post('/generate-rubbish/', (req, res) => {
+    const person = req.body.person
+    res.render('index', { person })
+})
 app.listen(port, () => {
     console.log(`express server is running on http://localhost:${port}`)
 })
---
+++ generate_rubbish.js
@@ -0,0 +1,5 @@
+function generateRubbish(){
+    console.log('sss')
+}
+
+generateRubbish()
